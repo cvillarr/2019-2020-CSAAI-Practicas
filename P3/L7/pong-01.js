4c31fe87ea00522e2a2982c39879b28ec43a1164
cvillarr <c.villarr@alumnos.urjc.es>
2020-03-14 19:11:38 +0100

    pruebas de pong hasta la red

---
+++ P3/L7/pong-01.js
@@ -0,0 +1,10 @@
+console.log("Ejecutando JS...");
+
+//-- Obtener el objeto canvas
+const canvas = document.getElementById("canvas");
+
+//-- Sus dimensiones las hemos fijado en el fichero
+//-- HTML. Las imprimimos en la consola
+console.log(`canvas: Anchura: ${canvas.width}, Altura: ${canvas.height}`);
+
+const ctx = canvas.getContext("2d");
